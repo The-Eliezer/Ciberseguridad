72de2d35a577e7759a5cca485d04fbe2fc2e5435
The-Eliezer <thepasifico>
2025-11-05 13:00:21 -0400

    Tipo de Dato

---
+++ Estructuras de Controles/SumaNumeros.py
@@ -0,0 +1,5 @@
+
+#Solicita dos números enteros y muestra la suma
+numero1 = int(input("Introduce el primer número entero: "))
+numero2 = int(input("Introduce el segundo número entero: "))
+print("La suma de", numero1, "y", numero2, "es", numero1 + numero2)
---
+++ Estructuras de Controles/mostrardoble-numero.py
@@ -0,0 +1,3 @@
+# Pide al usuario un número entero y muestra su doble
+numero = int(input("Introduce un número entero: "))
+print("El doble de", numero, "es", numero * 2)
