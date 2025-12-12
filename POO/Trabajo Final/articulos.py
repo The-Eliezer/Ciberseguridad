87d8ec4f6c2af29df77306dad850b90e15f66c08
The-Eliezer <thepasifico@gmail.com>
2025-12-12 19:17:08 -0400

    Add files via upload

---
+++ POO/Trabajo Final/articulos.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+class Articulos:
+
+    def abrir(self):
+        conexion=mysql.connector.connect(host="localhost", 
+                                              user="root", 
+                                              passwd="", 
+                                              database="bd1")
+        return conexion
+
+
+    def alta(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="insert into articulos(descripcion, precio) values (%s,%s)"
+        cursor.execute(sql, datos)
+        
+        cone.commit()
+        cone.close()
+
+    def consulta(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="select descripcion, precio from articulos where codigo=%s"
+        cursor.execute(sql, datos)
+        resultado=cursor.fetchall()
+        cone.close()
+        return resultado
+
+    def recuperar_todos(self):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="select codigo, descripcion, precio from articulos"
+        cursor.execute(sql)
+        resultado=cursor.fetchall()
+        cone.close()
+        return resultado
